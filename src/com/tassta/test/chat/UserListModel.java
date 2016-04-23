637450630f1d1c6fd4d4bde9af439780246bfb4c
SevostyanovGerman <sevostyanovg.d@gmail.com>
2016-04-23 17:50:20 +0300

    commit based src files

---
+++ src/com/tassta/test/chat/UserListModel.java
@@ -0,0 +1,11 @@
+package com.tassta.test.chat;
+
+import javafx.collections.ObservableList;
+
+/**
+ * User list model.
+ */
+public interface UserListModel
+{
+    ObservableList<User> getUserList();
+}
